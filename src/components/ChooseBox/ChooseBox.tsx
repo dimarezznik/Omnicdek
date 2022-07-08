c4a3f34ad923a4889591129ed83bc0346d7b0af0
Dima <47790594+dimarezznik@users.noreply.github.com>
2022-07-08 16:26:50 +0300

    Продолжение верстки страницы выбора упаковки

--- src/components/ChooseBox/ChooseBox.tsx
+++ src/components/ChooseBox/ChooseBox.tsx
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 import s from "./ChooseBox.module.scss";
 import GreenBtn from "../UI/GreenBtn/GreenBtn";
 import WhiteBtn from "../UI/WhiteBtn/WhiteBtn";
 import { NavLink } from "react-router-dom";
 
 const ChooseBox = () => {
+  const Test = async () => {
+    try {
+      const res = await fetch(
+        "https://dev.hub.omnic.solutions/api/2.0.0/public/device/34670a76-6022-4df5-ac1d-68f88589c904/ "
+      );
+      return res.json();
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const Test2 = async () => {
+    try {
+      const res = await fetch(
+        "https://dev.hub.omnic.solutions/api/2.0.0/public/orders/device/34670a76-6022-4df5-ac1d-68f88589c904/cells/statuses/?type=5"
+      );
+      return res.json();
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    Test().then((r) => console.log(r));
+    Test2().then((r) => console.log(r));
+  }, []);
+
   return (
     <div className={s.choose_text}>
       <span className={s.text}>Выберите размер посылки</span>
