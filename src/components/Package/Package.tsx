213a095fa1ca13a0625d122034443dd3ed2d5cfd
Dima <47790594+dimarezznik@users.noreply.github.com>
2022-07-08 15:06:07 +0300

    Разбиение компоненты подтверждения на подкомпоненты

--- src/components/Package/Package.tsx
+++ src/components/Package/Package.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import Pack from "../../assets/images/package.svg";
 import s from "./Package.module.scss";
-import { NavLink } from "react-router-dom";
+import OptionsPack from "./OptionsPack/OptionsPack";
+import ButtonsPack from "./ButtonsPack/ButtonsPack";
 
 const Package = () => {
   return (
@@ -10,27 +10,8 @@ const Package = () => {
         <div className={s.send_text}>
           <span>Отправление должно быть надежно упаковано</span>
         </div>
-        <div className={s.instruction_packing}>
-          <div>
-            <img src={Pack} alt="" />
-          </div>
-          <ul className={s.instruction_text}>
-            <li>
-              Упакуйте товар в коробку или пакет. На упаковке не должно быть
-              посторонних стикеров и этикеток;
-            </li>
-            <li>
-              Заклейте упаковку скотчем, чтобы не было доступа к вложенным
-              предметам;
-            </li>
-          </ul>
-        </div>
-        <div className={s.buttons}>
-          <NavLink to={"/"}>
-            <button className={s.back}>Назад</button>
-          </NavLink>
-          <button className={s.agree}>Я хорошо упаковал</button>
-        </div>
+        <OptionsPack />
+        <ButtonsPack />
       </div>
     </section>
   );
